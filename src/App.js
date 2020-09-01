e2be38127baec727bbe13d42d6bae054bbe3dfa1
faqih mubarak <faqihmubarak666@gmail.com>
2020-09-01 10:28:23 +0700

    Landing Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
 import Header from "./component/Header";
-import Login from "./component/Login";
+import Login from "./component/pages/Login";
+import Navbar from "./component/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./component/pages/Home";
+import Products from "./component/pages/Products";
+import Services from "./component/pages/Services";
 
 function App() {
   return (
     <div>
-      {/* <Header /> */}
-      <Login />
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/services" exact component={Services} />
+          <Route path="/products" exact component={Products} />
+          <Route path="/login" exact component={Login} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/component/pages/Login.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import {
+  FacebookLoginButton,
+  GoogleLoginButton,
+} from "react-social-login-buttons";
+import "../../style/Login.css";
+
+class Login extends Component {
+  render() {
+    return (
+      <div className="container-login">
+        <Form className="login-form">
+          <h2>PLAY UP!</h2>
+          <FormGroup>
+            <Label>Username / Email</Label>
+            <Input type="email" placeholder="Username / Email" />
+          </FormGroup>
+          <FormGroup>
+            <Label>Password</Label>
+            <Input type="password" placeholder="Password" />
+          </FormGroup>
+          <Button
+            className="btn-lg btn-dark btn-block"
+            style={{ backgroundColor: "#2c3c5b" }}
+          >
+            Masuk
+          </Button>
+          <div className="text-center pt-3">atau</div>
+          <FacebookLoginButton className="mt-3 mb-3" />
+          <GoogleLoginButton className="mt-3 mb-3" />
+          <div className="text-center">
+            <span>Tidak punya akun ? </span>
+            <a href="/register">Buat akun</a>
+          </div>
+        </Form>
+      </div>
+    );
+  }
+}
+
+export default Login;
